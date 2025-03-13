fb0e35607b39bc57a702802a47b153d6e4691850
nebuzerom <nebuzeromk@gmail.com>
2025-03-13 14:30:14 -0400

    deploy

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const axiosInstance=axios.create({
-
-    baseURL:"http://127.0.0.1:5001/e-clone-10893/us-central1/api",
-})
+const axiosInstance = axios.create({
+  // baseURL:"http://127.0.0.1:5001/e-clone-10893/us-central1/api",
+  baseURL: "https://us-central1-e-clone-10893.cloudfunctions.net/api",
+});
 
 export {axiosInstance}
